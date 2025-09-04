a4e1bec06d8929cc37797b4ddd4600f27e1f0070
Justin Lee <ju3tinlee@gmail.com>
2025-09-03 22:03:14 -0700

    Added get_nature method

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import get_evolution_line from './PokeAPI/get_evolution_line.js';
 import get_generation from './PokeAPI/get_generation.js';
 import get_item from './PokeAPI/get_item.js';
 import get_move from './PokeAPI/get_move.js';
+import get_nature from './PokeAPI/get_nature.js';
 import get_pokemon from './PokeAPI/get_pokemon.js';
 import get_pokemon_species from './PokeAPI/get_pokemon_species.js';
 import get_type from './PokeAPI/get_type.js';
@@ -13,6 +14,7 @@ export default {
     get_evolution_line,
     get_type,
     get_ability,
+    get_nature,
     get_move,
     get_item,
     get_generation
